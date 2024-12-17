be73874b94c0e3ab01d3528a08f69cc5a016a5ac
Samyrrrrrr990 <samyar.shafiee2009@gmail.com>
2024-12-16 21:20:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navButton = document.getElementById('navButton');
+    let active = false;
 
+    navButton.addEventListener('click', function() {
+        active = !active;
+        if (active) {
+            navButton.classList.add('active');
+        } else {
+            navButton.classList.remove('active');
+        }
+    });
+});
